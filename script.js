41333f746347380e920d7495eb3ce638dad604ff
FreedomCrue <130007416+FreedomCrue@users.noreply.github.com>
2023-12-04 20:39:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-ДМИП.рф/api2.php
-_GET params:
-
-base64 login
-base64 password
-int app = 112233
+const func = async () => {
+    const encodedLogin = encodeURIComponent("aW5kcmEua2RwYWVnaXNAbWFpbC5ydQ==");
+    const encodedPassword = encodeURIComponent("cXdpbDYwOV9sYU4=");
+    const url = `https://дмип.рф/api2.php?login=${encodedLogin}&password=${encodedPassword}&app=112233&type=auth`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      console.log(data);
+  
+      document.body.innerHTML = `<h1>${data.title}</h1><p></p>`;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
+  func();
